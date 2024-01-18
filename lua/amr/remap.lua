ad99c16c3116d450ddb6d54c4aab2da160181bda
3mraz <amr.jaafar3321@gmail.com>
2024-01-18 15:47:39 +0100

    changed add line above map

--- lua/amr/remap.lua
+++ lua/amr/remap.lua
@@ -1,7 +1,10 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 vim.keymap.set("n", "<C-[>", "o<Esc>k")
-vim.keymap.set("n", "<C-x>", "O<Esc>j")
+vim.keymap.set("n", "<C-b>", "O<Esc>j")
+
+
+
 
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
